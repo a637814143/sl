a1e20da4603fad892e592206ae80b537f35dcab1
a637814143 <140132912+a637814143@users.noreply.github.com>
2025-11-13 14:58:52 +0800

    Fix startup failures by aligning application components

--- demo/src/main/java/com/eightam/lab/web/DrinkCatalogController.java
+++ demo/src/main/java/com/eightam/lab/web/DrinkCatalogController.java
@@ -2,21 +2,20 @@ package com.eightam.lab.web;
 
 import com.eightam.lab.dto.DrinkSummary;
 import com.eightam.lab.service.DrinkCatalogService;
+import java.util.List;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/drinks")
 @CrossOrigin
-public class DrinkController {
+public class DrinkCatalogController {
 
     private final DrinkCatalogService drinkCatalogService;
 
-    public DrinkController(DrinkCatalogService drinkCatalogService) {
+    public DrinkCatalogController(DrinkCatalogService drinkCatalogService) {
         this.drinkCatalogService = drinkCatalogService;
     }
 
